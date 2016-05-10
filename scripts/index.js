1144ab0bd5e38a242792c957e6458b363dc0101f
Dan Tong <danieltongnz@gmail.com>
2016-05-10 16:06:48 +1200

    Remove pricing and 'register your interest' sections, speed up 'learn more' scroll

--- scripts/index.js
+++ scripts/index.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
     
   });
 
-  $(".learn-more").click(function() {
+  $('.learn-more').click(function() {
     $('html,body').animate({
         scrollTop: $("#manage").offset().top - 100},
-        'slow');
+        'fast');
   });
 });
